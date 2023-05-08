8ab42754c80f2a93924fc8c401087f05068612bb
User <User@MIRV-PC>
2023-05-08 12:49:53 +0200

    Add project files.

---
+++ MVCKalkulacka/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCKalkulacka.Models;
+
+namespace MVCKalkulacka.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index(string jmeno)
+        {
+            Kalkulacka kalkulacka = new Kalkulacka();
+            ViewBag.Jmeno = jmeno;
+            return View(kalkulacka);
+        }
+        [HttpPost]
+        public IActionResult Index(Kalkulacka kalkulacka)
+        {
+            if (ModelState.IsValid)
+            {
+                kalkulacka.VypocitejVysledek();
+            }
+            return View(kalkulacka);
+        }
+    }
+}
---
+++ MVCKalkulacka/Models/Kalkulacka.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCKalkulacka.Models
+{
+    public class Kalkulacka
+    {
+        [Display(Name = "1.číslo" )]
+        [Range(1,100, ErrorMessage ="Zadejte prosím číslo od 1 do 100.")]
+        public int PrvniCislo { get; set; }
+        [Display(Name = "2.číslo")]
+        [Range(1, 100, ErrorMessage = "Zadejte prosím číslo od 1 do 100.")]
+        public int DruheCislo { get; set; } 
+        public double Vysledek { get; private set; }
+        [Display(Name = "Operace" )]
+        public string Operace { get; set; }
+
+        public List <SelectListItem> MozneOperace { get; private set; }
+
+        public Kalkulacka()
+        {
+            Operace = "+";
+            MozneOperace = new List<SelectListItem>
+            {
+                new SelectListItem {Text = "Sečti", Value = "+", Selected = true },
+                new SelectListItem {Text = "Odečti", Value = "-",},
+                new SelectListItem {Text = "Vynásiob", Value = "*", },
+                new SelectListItem {Text = "Vyděl", Value = "/", },
+            };
+        }
+        public void VypocitejVysledek()
+        {
+            Vysledek = Operace switch
+            {
+                "+" => PrvniCislo + DruheCislo,
+                "-" => PrvniCislo - DruheCislo,
+                "*" => PrvniCislo * DruheCislo,
+                "/" => PrvniCislo / DruheCislo,
+                _=> 0
+
+            };
+        }
+
+    }
+}
